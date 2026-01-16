72a84c9a1ea466bc6f2deb215b176b3f83f5a823
ShumpeiKawamura37 <lrivermht1611flat@icloud.com>
2026-01-17 00:07:18 +0900

    桁数制限を修正

--- src/main/java/utility/FormatterUtil.java
+++ src/main/java/utility/FormatterUtil.java
@@ -5,7 +5,7 @@ import java.math.MathContext;
 import java.math.RoundingMode;
 
 public class FormatterUtil {
-  
+
   public static String formatForDisplay(BigDecimal v, int maxDigits) {
     String string = v.toString();
     // 整数と少数を個別に管理し、処理を通した後連結させる
@@ -15,45 +15,43 @@ public class FormatterUtil {
     int integerCount = 0;
 
     int digitCount = 0;
-    for(int i = 0; i < string.length(); i ++) {
-      if(Character.isDigit(string.charAt(i))) {
-        digitCount ++;
+    for (int i = 0; i < string.length(); i++) {
+      if (Character.isDigit(string.charAt(i))) {
+        digitCount++;
         // 整数なら整数桁数のカウントも追加
-        if(isInteger) {
+        if (isInteger) {
           integers.append(string.charAt(i));
-        } else if(!isInteger) {
+        } else if (!isInteger) {
           decimals.append(string.charAt(i));
         }
       }
 
       // 小数点に達したら整数桁数のカウントは止める
-      if(string.charAt(i) == '.') {
+      if (string.charAt(i) == '.') {
         isInteger = false;
       }
     }
     // 8桁以内ならそのまま返す
-    if(digitCount <= maxDigits) {
+    if (digitCount <= maxDigits) {
       return string;
     }
 
     // 少数部分の余剰な0を排除
     BigDecimal bd = new BigDecimal(decimals.toString());
     BigDecimal decimalsRemovedExtraZeros = bd.stripTrailingZeros();
-    
 
-    //decimalsRemovedExtraZerosとintegersを連結させて、下記にroundする
+    // decimalsRemovedExtraZerosとintegersを連結させて、下記にroundする
+    BigDecimal linked = new BigDecimal(integers.toString() + decimalsRemovedExtraZeros.toString());
 
-    // BigDecimal formattedDecimals =  decimalsRemovedExtraZeros.round(new MathContext(maxDigits, RoundingMode.HALF_UP));
+    BigDecimal formatted = linked.round(new MathContext(maxDigits, RoundingMode.HALF_UP));
 
     // formattedは小数点以下の文字数しか制限できなかったので、整数＋小数点以下が８文字以下になるように手動で余剰分をカットする
     StringBuilder sb = new StringBuilder();
-    if((integerCount + decimalsRemovedExtraZeros) >= maxDigits) {
-      for(int i = 0; i <= 8; i ++) {
-        sb.append(連結後.toString().charAt(i));
+    if (formatted.toString().length() >= maxDigits) {
+      for (int i = 0; i <= 8; i++) {
+        sb.append(formatted.toString().charAt(i));
       }
     }
-
-  
     return sb.toString();
   }
 }
